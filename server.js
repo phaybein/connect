fea21a1f78e12ad66acb881ffef58407e8b1eb48
phaybein <fabian.m.martinez@gmail.com>
2019-01-21 08:25:44 -0800

    connected to mongodb RE #mongodb #connect

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// INITIALIZE EXPRESS
 const app = express();
 
+// DB CONFIG
+const db = require('./config/keys').mongoURI;
+
+// CONNECT TO MONGODB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('Mongodb Connected'))
+  .catch(err => console.log(err));
+
+// ROUTES
 app.get('/', (req, res) => res.send('Hi!'));
 
+// PORT
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
