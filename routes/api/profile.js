850370e8e00067bb08b55e9f7ff88ba746b106dd
phaybein <fabian.m.martinez@gmail.com>
2019-01-23 09:24:41 -0800

    created profile to get current user RE #profile #user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// LOAD PROFILE MODEL
+const Profile = require('../../models/Profile');
+
+// LOAD USER PROFILE
+const User = require('../../models/User');
 
 // @ROUTE GET API/PROFILE/TEST
 // @DESC TESTS PROFILE ROUTE
@@ -9,5 +17,29 @@ router.get('/test', (req, res) => {
     msg: 'Profile works'
   });
 });
+
+// @ROUTE GET API/PROFILE
+// @DESC GET CURRENT USER PROFILE
+// @ACCESS PRIVATE
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // INITIALIZE ERRORS
+    const errors = {};
+
+    Profile.findOne({
+      user: req.user.id
+    })
+      .then(profile => {
+        if (!profile) {
+          errors.noProfile = 'No profile has been found for this user';
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
