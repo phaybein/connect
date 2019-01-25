93b547c5a576456e8f4cb641f26ff700a959220c
phaybein <fabian.m.martinez@gmail.com>
2019-01-25 09:02:54 -0800

    added delete route for individual post RE #delete #route #single

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
-//  BRING IN POST MODEL
+//  BRING IN MODELS
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 // BRING IN VALIDATION
 const validatePostInput = require('../../validation/post');
@@ -16,6 +17,32 @@ router.get('/test', (req, res) => {
   res.json({
     msg: 'Posts works'
   });
+});
+
+// @ROUTE GET API/POSTS
+// @DESC GET ALL POSTS
+// @ACCESS PUBLIC
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({
+      date: -1
+    })
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(404).json({ posts: 'Could not locate any posts' })
+    );
+});
+
+// @ROUTE GET API/POSTS/:ID
+// @DESC GET SINGLE POSTS BY ID
+// @ACCESS PUBLIC
+router.get('/:id', (req, res) => {
+  const errors = {};
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ post: 'Could not locate post with that ID' })
+    );
 });
 
 // @ROUTE POST API/POSTS
@@ -43,6 +70,33 @@ router.post(
 
     // SAVE POST
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @ROUTE DELETE API/POSTS/:ID
+// @DESC DELETE SPECIFIC POSTS
+// @ACCESS PRIVATE
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({
+      user: req.user.id
+    }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // CHECK FOR POST OWNER
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notAuthorized: 'User is not authorized' });
+          }
+
+          // DELETE
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postNotFound: 'No post found' }));
+    });
   }
 );
 
