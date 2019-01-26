de1719e4d65975c7b5864fbd37930d36488aee34
phaybein <fabian.m.martinez@gmail.com>
2019-01-25 21:50:01 -0800

    added some typography for responsive fonts RE #typography #responsive #fonts

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 // COMPONENTS
 import Footer from './components/layout/footer/Footer';
--- client/src/components/layout/footer/Footer.js
+++ client/src/components/layout/footer/Footer.js
@@ -4,7 +4,7 @@ export class Footer extends Component {
   render() {
     return (
       <footer class="bg-dark text-white mt-5 p-4 text-center">
-        Copyright &copy; 2018 Connect
+        Copyright &copy; {new Date().getFullYear()} Connect
       </footer>
     );
   }
