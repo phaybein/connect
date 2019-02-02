a18dbf2b6dda16822efb6b922608b3e0f3cd37ed
phaybein <fabian.m.martinez@gmail.com>
2019-02-02 06:46:39 -0800

    added login action with redux and set current user RE #login #set-user #redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
-import { GET_ERRORS } from './types';
 import axios from 'axios';
+import setAuthToken from '../utils/setkAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // REGISTER
 export const registerUser = (userData, history) => dispatch => {
@@ -13,4 +16,40 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// LOGIN / GET USER TOKEN
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      // GRAB TOKEN
+      const { token } = res.data;
+
+      // SET TOKEN TO LOCAL STORAGE
+      localStorage.setItem('jwtToken', token);
+
+      // SET TOKEN TO AUTH HEADER
+      setAuthToken(token);
+
+      // DECODE TOKEN & GET USER
+      const decoded = jwt_decode(token);
+
+      // SET CURRENT USER
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// SET LOGGED IN USER
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
